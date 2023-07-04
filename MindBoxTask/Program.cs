fe2ed060e9e73bad481fa2552e393fa9b73354d6
Hihiz <public@mail.ru>
2023-07-04 20:51:23 +0300

    Lib и тесты

--- MindBoxTask/Program.cs
+++ MindBoxTask/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MindBoxTask.Lib;
+
+Circle circle = new Circle(10);
+Console.WriteLine(circle.CalcArea());
+
+Triangle triangle = new Triangle(3, 8, 9);
+Console.WriteLine(triangle.CalcArea());
